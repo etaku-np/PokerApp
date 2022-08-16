a77794621384ea38fa701f496ae538300ab5f4d9
遠藤 拓 <e.taku@mac0359.np.co.jp>
2022-08-16 16:52:06 +0900

    Add a check method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/" => "home#top"
-
+  post "home/check" => "home#check"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
