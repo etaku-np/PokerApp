34770963c0f96c7a56fe7b5edc844d2ea4ad23fe
etaku74r74n <etaku@netprotections.co.jp>
2022-08-30 18:26:50 +0900

    incorrect_card_errorをincorrect_card_arrayに変更しました。

--- app/services/errors.rb
+++ app/services/errors.rb
@@ -8,7 +8,7 @@ module Errors
       ["文字列を入力してください。"]
     else
       card_array = cards.split
-      input_error(card_array) || incorrect_card_error(card_array) || duplicate_error(card_array)
+      input_error(card_array) || incorrect_card_array(card_array) || duplicate_error(card_array)
     end
   end
 
@@ -28,19 +28,19 @@ module Errors
   end
 
   # 不正なカードに対するエラー文を配列で出す。
-  def incorrect_card_error(card_array)
+  def incorrect_card_array(card_array)
     error_msg_array = []
     card_array.each.with_index do |card, i|
-      error_msg_array.push("#{i + 1}番目のカード指定文字が不正です。(#{card})") if incorrect_card_error?(card)
+      error_msg_array.push("#{i + 1}番目のカード指定文字が不正です。(#{card})") if incorrect_card_array?(card)
     end
     error_msg_array.blank? ? nil : error_msg_array
   end
 
   # 不正なカードがあるか？
-  def incorrect_card_error?(card)
+  def incorrect_card_array?(card)
     true if card !~ CORRECT
   end
 
-  module_function :search_errors, :input_error, :class_error?, :duplicate_error, :incorrect_card_error, :incorrect_card_error?
+  module_function :search_errors, :input_error, :class_error?, :duplicate_error, :incorrect_card_array, :incorrect_card_array?
 end
 
