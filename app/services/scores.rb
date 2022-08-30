433c795be1d3fe60ee84f37283d2278f9bf789ee
etaku74r74n <etaku@netprotections.co.jp>
2022-08-30 17:46:16 +0900

    空白行を削除しました。

--- app/services/scores.rb
+++ app/services/scores.rb
@@ -1,14 +1,9 @@
 module Scores
   def search_best(score_array, results)
-
     score_array.each_with_index do |_, i|
       results[i]["best"] == score_array.max ? results[i]["best"] = true : results[i]["best"] = false
     end
-
     results
-
   end
-
   module_function :search_best
-
 end
