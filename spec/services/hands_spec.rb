805ca6f9c9ffcd27540303bf5ca8a0861119be21
etaku74r74n <etaku@netprotections.co.jp>
2022-09-01 11:22:00 +0900

    テストコードを圧縮し、読みやすくしました。

--- spec/services/hands_spec.rb
+++ spec/services/hands_spec.rb
@@ -4,101 +4,95 @@ RSpec.describe Hands, type: :service do
   include Hands
 
   describe "#search_hands" do
-    describe "hand's name" do
-      subject { Hands.search_hands(cards)[:name] }
+    let(:search_hands){ Hands.search_hands(cards) }
 
-      context "when the hand is Straight Flush" do
-        let(:cards) { "C5 C6 C4 C3 C7" }
-        it { is_expected.to eq "ストレートフラッシュ" }
+    context "when the hand is Straight Flush" do
+      let(:cards) { "H13 H1 H11 H12 H10" }
+      it "returns \"ストレートフラッシュ\"" do
+        expect(search_hands[:name]).to eq "ストレートフラッシュ"
       end
-
-      context "when the hand is Straight" do
-        let(:cards) { "C12 H13 D10 H1 H11" }
-        it { is_expected.to eq "ストレート" }
-      end
-
-      context "when the hand is Flush" do
-        let(:cards) { "D7 D4 D10 D2 D3" }
-        it { is_expected.to eq "フラッシュ" }
-      end
-
-      context "when the hand is Four of a kind" do
-        let(:cards) { "S8 H8 D8 C8 C12" }
-        it { is_expected.to eq "フォーカード" }
-      end
-
-      context "when the hand is Full House" do
-        let(:cards) { "D5 S10 D10 H5 C5" }
-        it { is_expected.to eq "フルハウス" }
-      end
-
-      context "when the hand is Three of a kind" do
-        let(:cards) { "C7 S7 S11 C3 H7" }
-        it { is_expected.to eq "スリーカード" }
-      end
-
-      context "when the hand is Two pair" do
-        let(:cards) { "H9 H4 S9 C5 D4" }
-        it { is_expected.to eq "ツーペア" }
-      end
-
-      context "when the hand is Pair" do
-        let(:cards) { "H12 H5 D12 S6 S10" }
-        it { is_expected.to eq "ワンペア" }
-      end
-
-      context "when the hand is High card" do
-        let(:cards) { "D4 H8 S1 C10 D9" }
-        it { is_expected.to eq "ハイカード" }
+      it "returns 8" do
+        expect(search_hands[:score]).to eq 8
       end
     end
 
-    describe "hand's score" do
-      subject { Hands.search_hands(cards)[:score] }
-
-      context "when the hand is Straight Flush" do
-        let(:cards) { "H13 H1 H11 H12 H10" }
-        it { is_expected.to eq 8 }
+    context "when the hand is Four of a kind" do
+      let(:cards) { "S13 H13 D13 C8 C13" }
+      it "returns \"フォーカード\"" do
+        expect(search_hands[:name]).to eq "フォーカード"
       end
-
-      context "when the hand is Four of a kind" do
-        let(:cards) { "S13 H13 D13 C8 C13" }
-        it { is_expected.to eq 7 }
+      it "returns 7" do
+        expect(search_hands[:score]).to eq 7
       end
+    end
 
-      context "when the hand is Full House" do
-        let(:cards) { "D8 S2 C8 H8 C2" }
-        it { is_expected.to eq 6 }
+    context "when the hand is Full House" do
+      let(:cards) { "D8 S2 C8 H8 C2" }
+      it "returns \"フルハウス\"" do
+        expect(search_hands[:name]).to eq "フルハウス"
       end
-
-      context "when the hand is Flush" do
-        let(:cards) { "S1 S4 S11 S2 S3" }
-        it { is_expected.to eq 5 }
+      it "returns 6" do
+        expect(search_hands[:score]).to eq 6
       end
+    end
 
-      context "when the hand is Straight" do
-        let(:cards) { "S2 H3 D1 H4 C5" }
-        it { is_expected.to eq 4 }
+    context "when the hand is Flush" do
+      let(:cards) { "S1 S4 S11 S2 S3" }
+      it "returns \"フラッシュ\"" do
+        expect(search_hands[:name]).to eq "フラッシュ"
       end
-
-      context "when the hand is Three of a kind" do
-        let(:cards) { "H7 S9 D9 C3 H9" }
-        it { is_expected.to eq 3 }
+      it "returns 5" do
+        expect(search_hands[:score]).to eq 5
       end
+    end
 
-      context "when the hand is Two pair" do
-        let(:cards) { "C4 H7 H11 C7 D4" }
-        it { is_expected.to eq 2 }
+    context "when the hand is Straight" do
+      let(:cards) { "S2 H3 D1 H4 C5" }
+      it "returns \"ストレート\"" do
+        expect(search_hands[:name]).to eq "ストレート"
       end
-
-      context "when the hand is Pair" do
-        let(:cards) { "H10 H5 D13 S6 S10" }
-        it { is_expected.to eq 1 }
+      it "returns 4" do
+        expect(search_hands[:score]).to eq 4
       end
+    end
 
-      context "when the hand is High card" do
-        let(:cards) { "D4 H8 S1 C10 D9" }
-        it { is_expected.to eq 0 }
+    context "when the hand is Three of a kind" do
+      let(:cards) { "H7 S9 D9 C3 H9" }
+      it "returns \"スリーカード\"" do
+        expect(search_hands[:name]).to eq "スリーカード"
+      end
+      it "returns 3" do
+        expect(search_hands[:score]).to eq 3
+      end
+    end
+
+    context "when the hand is Two pair" do
+      let(:cards) { "C4 H7 H11 C7 D4" }
+      it "returns \"ツーペア\"" do
+        expect(search_hands[:name]).to eq "ツーペア"
+      end
+      it "returns 2" do
+        expect(search_hands[:score]).to eq 2
+      end
+    end
+
+    context "when the hand is Pair" do
+      let(:cards) { "H10 H5 D13 S6 S10" }
+      it "returns \"ワンペア\"" do
+        expect(search_hands[:name]).to eq "ワンペア"
+      end
+      it "returns 1" do
+        expect(search_hands[:score]).to eq 1
+      end
+    end
+
+    context "when the hand is High card" do
+      let(:cards) { "D4 H8 S1 C10 D9" }
+      it "returns \"ハイカード\"" do
+        expect(search_hands[:name]).to eq "ハイカード"
+      end
+      it "returns 0" do
+        expect(search_hands[:score]).to eq 0
       end
     end
   end
