63a6e5c0d0efefe00d586960a682c8cb0bc2a569
etaku74r74n <etaku@netprotections.co.jp>
2022-09-01 11:40:13 +0900

    コメントを一部修正しました。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,7 @@ class HomeController < ApplicationController
     # エラーメッセージがあればそれを、なければ（何かの役になっているはずなので）役の判定を出す。
     message = Errors.search_errors(cards) || Hands.search_hands(cards)[:name]
 
-    # （特にエラーメッセージが）配列になっている場合は改行して出す。そうでなければそのまま。
+    # 配列になっている場合は改行して出す。そうでなければそのまま。
     flash[:message] = message.is_a?(Array) ? message.join("\n") : message
     redirect_to("/")
   end
