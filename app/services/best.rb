8dc6e2488c6ae35a69933a0faacc52326b8077ab
etaku74r74n <etaku@netprotections.co.jp>
2022-09-02 18:38:04 +0900

    コードを平易に修正しました。

--- app/services/best.rb
+++ app/services/best.rb
@@ -1,7 +1,9 @@
 module Best
   def search_best(score_array, results)
     score_array.each_with_index do |_, i|
-      results[i]["best"] == score_array.max ? results[i]["best"] = true : results[i]["best"] = false
+      best = results[i]["best"]
+      tf = best == score_array.max ? true : false
+      results[i]["best"] = tf
     end
     results
   end
