7d38cb7afce5af0e131918f058fdadad5f530b59
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 10:45:03 +0900

    不要なコメントを削除しました。

--- app/apis/api/ver1/root.rb
+++ app/apis/api/ver1/root.rb
@@ -1,12 +1,9 @@
 module API
   module Ver1
     class Root < Grape::API
-      # これでdomain/api/v1でアクセス出来るようになる。(versioning)
       version 'v1', using: :path
       format :json
-
       mount API::Ver1::Cards
-
     end
   end
 end
