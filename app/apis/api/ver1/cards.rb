e42dc7085607a29371cf0a23c02a73e50a82cedf
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 12:34:44 +0900

    インスタンス変数をローカル変数に変更しました。

--- app/apis/api/ver1/cards.rb
+++ app/apis/api/ver1/cards.rb
@@ -20,26 +20,26 @@ module API
 
           # エラーがないカードは、役を判定する
           correct_cards_set = cards_set.select { |cards| ErrorService.validate_cards(cards).nil? }
-          @results = correct_cards_set.map do |cards|
+          results = correct_cards_set.map do |cards|
             {
               "cards" =>  cards,
               "hand"  =>  HandService.judge_cards(cards)[:name],
               "best"  =>  HandService.judge_cards(cards)[:score]
             }
           end
-          @score_array = correct_cards_set.map{ |cards| HandService.judge_cards(cards)[:score] }
+          score_array = correct_cards_set.map{ |cards| HandService.judge_cards(cards)[:score] }
           # @resultsを、役最強部分を更新した状態に上書き
-          @results = BestService.judge_best(@score_array, @results)
+          results = BestService.judge_best(score_array, results)
 
           # エラーがあるコードは、エラーを判定する
           incorrect_cards_set = cards_set - correct_cards_set
-          @errors = incorrect_cards_set.map do |cards|
+          errors = incorrect_cards_set.map do |cards|
             {
               "cards" =>  cards,
               "msg"   =>  ErrorService.validate_cards(cards)
             }
           end
-          response = { "results" => @results, "errors" => @errors }
+          response = { "results" => results, "errors" => errors }
           status 200
           # 役またはエラーのみの場合、空の配列を表示しない
           response.delete_if{ |_, v| v.empty? }
