e68392c32a0b58663c0b7440716ba6df419c6a92
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 11:37:50 +0900

    ファイル名の修正に伴う対応を行いました。

--- app/apis/api/ver1/cards.rb
+++ app/apis/api/ver1/cards.rb
@@ -1,17 +1,9 @@
 module API
   module Ver1
     class Cards < Grape::API
-      include Hands
-      include Errors
-      include Best
-
-      format :json
-      default_format :json
-      content_type :json, "application/json"
-      content_type :xml, 'application/xml'
-      content_type :javascript, 'application/javascript'
-      content_type :txt, 'text/plain'
-      content_type :html, 'text/html'
+      include HandService
+      include ErrorService
+      include BestService
 
       params do
         requires :cards, type: Array
@@ -27,24 +19,24 @@ module API
           error!({ errors: [{ msg: "入力内容を確認してください。" }] }, 400) if cards_set.empty?
 
           # エラーがないカードは、役を判定する
-          correct_cards_set = cards_set.select { |cards| Errors.search_errors(cards).nil? }
+          correct_cards_set = cards_set.select { |cards| ErrorService.validate_cards(cards).nil? }
           @results = correct_cards_set.map do |cards|
             {
               "cards" =>  cards,
-              "hand"  =>  Hands.search_hands(cards)[:name],
-              "best"  =>  Hands.search_hands(cards)[:score]
+              "hand"  =>  HandService.judge_cards(cards)[:name],
+              "best"  =>  HandService.judge_cards(cards)[:score]
             }
           end
-          @score_array = correct_cards_set.map{ |cards| Hands.search_hands(cards)[:score] }
+          @score_array = correct_cards_set.map{ |cards| HandService.judge_cards(cards)[:score] }
           # @resultsを、役最強部分を更新した状態に上書き
-          @results = Best.search_best(@score_array, @results)
+          @results = BestService.judge_best(@score_array, @results)
 
           # エラーがあるコードは、エラーを判定する
           incorrect_cards_set = cards_set - correct_cards_set
           @errors = incorrect_cards_set.map do |cards|
             {
               "cards" =>  cards,
-              "msg"   =>  Errors.search_errors(cards)
+              "msg"   =>  ErrorService.validate_cards(cards)
             }
           end
           response = { "results" => @results, "errors" => @errors }
@@ -53,7 +45,6 @@ module API
           response.delete_if{ |_, v| v.empty? }
         end
       end
-
     end
   end
 end
--- app/apis/api/ver1/root.rb
+++ app/apis/api/ver1/root.rb
@@ -3,6 +3,12 @@ module API
     class Root < Grape::API
       version 'v1', using: :path
       format :json
+      default_format :json
+      content_type :json, "application/json"
+      content_type :xml, 'application/xml'
+      content_type :javascript, 'application/javascript'
+      content_type :txt, 'text/plain'
+      content_type :html, 'text/html'
       mount API::Ver1::Cards
     end
   end
