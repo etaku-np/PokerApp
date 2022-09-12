c969370b8c2eef6f1db184cfc6b1ad05b0474fcc
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 14:23:29 +0900

    入力重複時のバリデーションを追加しました。

--- app/apis/api/ver1/cards.rb
+++ app/apis/api/ver1/cards.rb
@@ -4,13 +4,16 @@ module API
       include HandService
       include ErrorService
       include BestService
+
       params do
         requires :cards, type: Array
       end
+
       resource :poker do
         post do
           cards_set = params[:cards]
           error!({ errors: [{ msg: "入力内容を確認してください。" }] }, 400) if cards_set.empty?
+          error!({ errors: [{ msg: "入力内容が重複しています。" }] }, 400) if cards_set != cards_set.uniq
 
           correct_cards_set = cards_set.select { |cards| ErrorService.validate_cards(cards).nil? }
           results = correct_cards_set.map do |cards|
@@ -21,7 +24,7 @@ module API
               "best"  =>  judge_result[:score]
             }
           end
-          score_array = correct_cards_set.map{ |cards| HandService.judge_cards(cards)[:score] }
+          score_array = results.map{ |result| result["best"] }
           results = BestService.judge_best(score_array, results)
 
           incorrect_cards_set = cards_set - correct_cards_set
