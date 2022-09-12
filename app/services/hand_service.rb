3c6ae8b44bcdd0dbec7da7044c9e5f1eaf5f0937
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 12:52:24 +0900

    インスタンス変数をローカル変数に変更しました。

--- app/services/hand_service.rb
+++ app/services/hand_service.rb
@@ -13,67 +13,63 @@ module HandService
   }
 
   def judge_cards(cards)
-    @suit_array = cards.scan(/[SHDC]/)
-    @num_array = cards.scan(/1[0-3]|[1-9]/).map(&:to_i).sort
-    judge
+    suit_array = cards.scan(/[SHDC]/)
+    num_array = cards.scan(/1[0-3]|[1-9]/).map(&:to_i).sort
+    num_count_array = (num_array.uniq.map{ |num| num_array.count(num) }).sort # ナンバーを数えて、それを配列にする
+    judge(suit_array, num_array, num_count_array)
   end
 
-  def flush?
-    true if @suit_array.uniq.length == 1
+  def flush?(suit_array)
+    true if suit_array.uniq.length == 1
   end
 
-  def straight?
-    true if ( (@num_array.uniq.length == 5) && (@num_array.max - @num_array.min == 4) ) || @num_array == [1, 10, 11, 12, 13]
+  def straight?(num_array)
+    true if ( (num_array.uniq.length == 5) && (num_array.max - num_array.min == 4) ) || num_array == [1, 10, 11, 12, 13]
   end
 
-  def combination # 同じナンバーを数えて配列で出す。
-    @num_count_array = (@num_array.uniq.map{ |num| @num_array.count(num) }).sort
+  def four_card?(num_count_array)
+    true if num_count_array == [1, 4]
   end
 
-  def four_card?
-    true if @num_count_array == [1, 4]
+  def full_house?(num_count_array)
+    true if num_count_array == [2, 3]
   end
 
-  def full_house?
-    true if @num_count_array == [2, 3]
+  def three_card?(num_count_array)
+    true if num_count_array == [1, 1, 3]
   end
 
-  def three_card?
-    true if @num_count_array == [1, 1, 3]
+  def two_pair?(num_count_array)
+    true if num_count_array == [1, 2, 2]
   end
 
-  def two_pair?
-    true if @num_count_array == [1, 2, 2]
+  def one_pair?(num_count_array)
+    true if num_count_array == [1, 1, 1, 2]
   end
 
-  def one_pair?
-    true if @num_count_array == [1, 1, 1, 2]
-  end
-
-  def judge
-    combination
-    if flush? && straight?
+  def judge(suit_array, num_array, num_count_array)
+    if flush?(suit_array) && straight?(num_array)
       HAND_SET[:straight_flash]
-    elsif four_card?
+    elsif four_card?(num_count_array)
       HAND_SET[:four_card]
-    elsif full_house?
+    elsif full_house?(num_count_array)
       HAND_SET[:full_house]
-    elsif flush?
+    elsif flush?(suit_array)
       HAND_SET[:flush]
-    elsif straight?
+    elsif straight?(num_array)
       HAND_SET[:straight]
-    elsif three_card?
+    elsif three_card?(num_count_array)
       HAND_SET[:three_card]
-    elsif two_pair?
+    elsif two_pair?(num_count_array)
       HAND_SET[:two_pair]
-    elsif one_pair?
+    elsif one_pair?(num_count_array)
       HAND_SET[:one_pair]
     else
       HAND_SET[:high_card]
     end
   end
 
-  module_function :judge_cards, :combination, :flush?, :straight?, :full_house?, :four_card?, :three_card?, :two_pair?, :one_pair?, :judge
+  module_function :judge_cards, :flush?, :straight?, :full_house?, :four_card?, :three_card?, :two_pair?, :one_pair?, :judge
 
 end
 
