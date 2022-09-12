b021da05ecad9ba117e899050fac9056d83789ec
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 10:34:00 +0900

    冗長なコードを修正しました。

--- app/services/best.rb
+++ app/services/best.rb
@@ -2,7 +2,7 @@ module Best
   def search_best(score_array, results)
     score_array.each_with_index do |_, i|
       score = results[i]["best"]
-      true_or_false = (score == score_array.max) ? true : false
+      true_or_false = (score == score_array.max)
       results[i]["best"] = true_or_false
     end
     results
