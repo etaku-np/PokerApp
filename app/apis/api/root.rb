e70c8024dc146f54d2b7bf57db2239f5600a4917
etaku74r74n <etaku@netprotections.co.jp>
2022-09-12 10:44:13 +0900

    不要なコメントを削除しました。

--- app/apis/api/root.rb
+++ app/apis/api/root.rb
@@ -1,14 +1,8 @@
 # require 'grape'
 module API
   class Root < Grape::API
-    # http://localhost:3000/api/
     prefix 'api'
     format :json
-
     mount API::Ver1::Root
-
-    # get do
-    # end
-
   end
 end
