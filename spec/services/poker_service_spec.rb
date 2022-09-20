8ccdbfe46903a2c5a401a1c0e41b153ca18c05b3
etaku74r74n <etaku@netprotections.co.jp>
2022-09-20 16:16:50 +0900

    無作為抽出からmapを用いたテストに変更

--- spec/services/poker_service_spec.rb
+++ spec/services/poker_service_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe PokerService, type: :service do
     let(:result){ PokerHand.judge_cards(cards)[:name] }
 
     context "with error, no hand" do
-      let(:cards){ "H6 C8 C1 D10" }
-      it "returns an error" do
-        expect(error||result).to eq error
+      context "when the number of cards is other than 5" do
+        let(:cards){ "H6 C8 C1 D10" }
+        it "returns an error" do
+          expect(error||result).to eq error
+        end
       end
     end
 
@@ -34,11 +36,11 @@ RSpec.describe PokerService, type: :service do
           "D3 S3 H12 H3 C12"
         ]
       }
-      it "returns 3 results" do
-        expect(results.length).to eq 3
+      it "returns correct results" do
+        expect(results.map{|result| result["hand"]}).to eq %w[ストレートフラッシュ ストレートフラッシュ フルハウス]
       end
       it "returns correct results" do
-        expect(results[2]["hand"]).to eq "フルハウス"
+        expect(results.map{|result| result["best"]}).to eq [true, true, false]
       end
       it "dose not display array of errors" do
         expect(errors).to eq nil
@@ -49,35 +51,35 @@ RSpec.describe PokerService, type: :service do
       let(:cards_set){
         [
           " ",
-          "c2 C30 f4 V5 v0",
+          "D2 C30 f4 D5 D10",
           "D3 D3 H12 H3 C12"
         ]
       }
-      it "returns 3 errors" do
-        expect(errors.length).to eq 3
-      end
       it "returns correct errors" do
-        expect(errors[0]["msg"]).to eq ["カードを入力してください。"]
-      end
-      it "returns correct errors" do
-        expect(errors[2]["msg"]).to eq ["カードが重複しています。"]
+        expect(errors.map{|error| error["msg"]}).to eq [["カードを入力してください。"], %w[2番目のカード指定文字が不正です。(C30) 3番目のカード指定文字が不正です。(f4)], ["カードが重複しています。"]]
       end
       it "dose not display array of results" do
         expect(results).to eq nil
       end
     end
 
-    context "when mixed cards are entered" do
+    context "when valid/invalid cards are entered" do
       let(:cards_set){
         [
           "D10 S10 C9 S8 H8",
           "C2 H6 D6 S6 C6",
-          "c2 C30 f4 V5 v0",
+          "F3 S4 C9 D8 H5",
           "D3 D3 H12 H3 C12"
         ]
       }
-      it "returns results and errors" do
-        expect(results.length + errors.length).to eq 4
+      it "returns correct hands" do
+        expect(results.map{|result| result["hand"]}).to eq %w[ツーペア フォーカード]
+      end
+      it "returns correct bests" do
+        expect(results.map{|result| result["best"]}).to eq [false, true]
+      end
+      it "returns correct errors" do
+        expect(errors.map{|error| error["msg"]}).to eq [["1番目のカード指定文字が不正です。(F3)"], ["カードが重複しています。"]]
       end
     end
   end
