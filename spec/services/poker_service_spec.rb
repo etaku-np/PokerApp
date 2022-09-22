7c59373d925b6983d481f73c9f6e97750d182a0a
etaku74r74n <etaku@netprotections.co.jp>
2022-09-22 09:31:49 +0900

    諸変更をテストに反映

--- spec/services/poker_service_spec.rb
+++ spec/services/poker_service_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe PokerService, type: :service do
 
   describe "#judge_results" do
     let(:error){ PokerTypo.check_typo_cards(cards) }
-    let(:result){ PokerHand.judge_cards(cards)[:name] }
+    let(:result){ PokerHand.judge(cards)[:name] }
 
     context "with error, no hand" do
       context "when the number of cards is other than 5" do
