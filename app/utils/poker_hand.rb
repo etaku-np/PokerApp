3da29d61154283b468e7711e1d6488ab0cc92025
etaku74r74n <etaku@netprotections.co.jp>
2022-09-26 13:13:21 +0900

    sendメソッドを導入

--- app/utils/poker_hand.rb
+++ app/utils/poker_hand.rb
@@ -1,7 +1,7 @@
 # 入力されたカードの役を判定
 module PokerHand
   HAND_SET = {
-    straight_flash: { name: "ストレートフラッシュ", score: 8 },
+    straight_flush: { name: "ストレートフラッシュ", score: 8 },
     four_card: { name: "フォーカード", score: 7 },
     full_house: { name: "フルハウス", score: 6 },
     flush: { name: "フラッシュ", score: 5 },
@@ -13,25 +13,11 @@ module PokerHand
   }
 
   def judge(cards)
-    if straight_flash?(cards)
-      HAND_SET[:straight_flash]
-    elsif four_card?(cards)
-      HAND_SET[:four_card]
-    elsif full_house?(cards)
-      HAND_SET[:full_house]
-    elsif flush?(cards)
-      HAND_SET[:flush]
-    elsif straight?(cards)
-      HAND_SET[:straight]
-    elsif three_card?(cards)
-      HAND_SET[:three_card]
-    elsif two_pair?(cards)
-      HAND_SET[:two_pair]
-    elsif one_pair?(cards)
-      HAND_SET[:one_pair]
-    else
-      HAND_SET[:high_card]
+    result_array = HAND_SET.keys.map(&:to_s).map do |x|
+      send(x, cards)
     end
+    # binding.pry
+    result_array.compact![0]
   end
 
   def suit_array(cards)
@@ -47,39 +33,43 @@ module PokerHand
     (num_array.uniq.map{ |num| num_array.count(num) }).sort
   end
 
-  def straight_flash?(cards)
-    flush?(cards) && straight?(cards)
+  def straight_flush(cards)
+    HAND_SET[:straight_flush] if ([straight(cards)] + [flush(cards)]).compact.length == 2
   end
 
-  def flush?(cards)
-    true if suit_array(cards).uniq.length == 1
+  def flush(cards)
+    HAND_SET[:flush] if suit_array(cards).uniq.length == 1
   end
 
-  def straight?(cards)
+  def straight(cards)
     num_array = num_array(cards)
-    true if ( (num_array.uniq.length == 5) && (num_array.max - num_array.min == 4) ) || num_array == [1, 10, 11, 12, 13]
+    HAND_SET[:straight] if ( (num_array.uniq.length == 5) && (num_array.max - num_array.min == 4) ) || num_array == [1, 10, 11, 12, 13]
   end
 
-  def four_card?(cards)
-    true if num_count_array(cards) == [1, 4]
+  def four_card(cards)
+    HAND_SET[:four_card] if num_count_array(cards) == [1, 4]
   end
 
-  def full_house?(cards)
-    true if num_count_array(cards) == [2, 3]
+  def full_house(cards)
+    HAND_SET[:full_house] if num_count_array(cards) == [2, 3]
   end
 
-  def three_card?(cards)
-    true if num_count_array(cards) == [1, 1, 3]
+  def three_card(cards)
+    HAND_SET[:three_card] if num_count_array(cards) == [1, 1, 3]
   end
 
-  def two_pair?(cards)
-    true if num_count_array(cards) == [1, 2, 2]
+  def two_pair(cards)
+    HAND_SET[:two_pair] if num_count_array(cards) == [1, 2, 2]
   end
 
-  def one_pair?(cards)
-    true if num_count_array(cards) == [1, 1, 1, 2]
+  def one_pair(cards)
+    HAND_SET[:one_pair] if num_count_array(cards) == [1, 1, 1, 2]
   end
 
-  module_function :suit_array, :num_array, :num_count_array, :straight_flash?, :flush?, :straight?, :full_house?, :four_card?, :three_card?, :two_pair?, :one_pair?, :judge
+  def high_card(cards)
+    HAND_SET[:high_card] unless ([straight(cards)] + [flush(cards)]).any? && num_count_array(cards) == [1, 1, 1, 1, 1]
+  end
+
+  module_function :suit_array, :num_array, :num_count_array, :straight_flush, :flush, :straight, :full_house, :four_card, :three_card, :two_pair, :one_pair, :high_card, :judge
 
 end
